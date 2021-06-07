b5407bdf6be25741cb85430b2f029e153129ab04
Squidfishrl <ivo.genchev4@gmail.com>
2021-06-07 22:00:05 +0300

    get_node_by_cords method + bug fixes

--- matrix.h
+++ matrix.h
@@ -45,6 +45,11 @@ struct matrix_t{
 
 /* FUNCTION DECLARATIONS */
 
+struct node_t* get_node_by_cords(struct matrix_t* matrix, int row, int column);
+int get_distance(int rowA, int columnA, int rowB, int columnB); // help function for get_node_by_cords
+int find_min(int* arr, int size); // help function for get_node_by_cords
+
+
 void print_matrix(struct matrix_t* matrix);
 void free_matrix(struct matrix_t* matrix);
 struct matrix_t* create_matrix(int rows, int columns);
@@ -54,6 +59,87 @@ struct node_t* init_node(int row, int column, struct node_t* up, struct node_t* 
 
 
 /* FUNCTION DEFINITIONS */
+
+// O(n) where n is the closest distance from the edge node to the searched for node
+struct node_t* get_node_by_cords(struct matrix_t* matrix, int row, int column){
+
+    struct node_t* searchNode;
+
+    // check if cords are valid
+    if(row > matrix->rows || row < 0){
+        return NULL;
+    }
+
+    if(column > matrix->columns || column < 0){
+        return NULL;
+    }
+
+    // I could automate vv but its only 4 numbs so it should be alright
+
+    // find best node to start search from
+    int blDist = get_distance(0, 0, row, column); // I could also use matrix->bottomLeftNode->row and column but it takes longer to write
+    int brDist = get_distance(0, matrix->columns-1, row, column);
+    int tlDist = get_distance(matrix->rows-1, 0, row ,column);
+    int trDist = get_distance(matrix->rows-1, matrix->columns-1, row, column);
+
+    int numbArr[] = {blDist, brDist, tlDist, trDist};
+    int min = find_min(numbArr, 4);
+
+    if(blDist == min){
+        searchNode = matrix->bottomLeftNode;
+    }else if(brDist == min){
+        searchNode = matrix->bottomRightNode;
+    }else if(tlDist == min){
+        searchNode = matrix->topLeftNode;
+    }else{
+        searchNode = matrix->topRightNode;
+    }
+
+    while(row != searchNode->row){
+        if(searchNode->row > row){
+            searchNode = searchNode->down;
+        }else if(searchNode->row < row){  // elif instead of if, cuz searchNode could become NULL and segmentation fault
+            searchNode = searchNode->up;
+        }
+    }
+
+    while(column != searchNode->column){
+        if(searchNode->column > column){
+            searchNode = searchNode->left;
+        }else if(searchNode->column < column){
+            searchNode = searchNode->right;
+        }
+    }
+
+    return searchNode;
+}
+
+// O(n), where n is array size
+int find_min(int* arr, int size){ // help function for get_node_by_cord
+
+    // find largest number by putting all the numbs in an array and iterating through it
+    int min = arr[0];
+
+    for(int i = 1; i < size; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+// O(1)
+int get_distance(int rowA, int columnA, int rowB, int columnB){
+    // calculate the required moves to reach a point (withouth diagonals)
+    int rowDist = rowA - rowB;
+    if(rowDist < 0) rowDist *= -1;
+
+    int columnDist = columnA - columnB;
+    if(columnDist < 0) columnDist *= -1;
+
+    return rowDist + columnDist;
+}
 
 // O(n), where n is the amount of nodes connected to the matrix
 void print_matrix(struct matrix_t* matrix){
@@ -131,7 +217,7 @@ struct matrix_t* create_matrix(int rows, int columns){
     struct node_t* tempNode2 = bottomLeftNode;
 
     // iterate through the columns
-    for(int column = 0; column < columns-1; column++){
+    for(int column = 0; column < columns; column++){
         // iterate through the rows
         for(int row = 1; row < rows; row++){
 
@@ -152,7 +238,7 @@ struct matrix_t* create_matrix(int rows, int columns){
                 tempNode1->left->up->right = tempNode1->up;
 
                 // add topRightNode to matrix
-                if(row == rows-1 && column == columns-2){
+                if(row == rows-1 && column == columns-1){
                     newMatrix->topRightNode = tempNode1->up;
                 }
             }
@@ -174,7 +260,9 @@ struct matrix_t* create_matrix(int rows, int columns){
         tempNode1 = tempNode2;
     }
 
-
+    // for some reason thare is always 1 extra node on row 0 to the right so I remove it
+    free(newMatrix->bottomRightNode->right);
+    newMatrix->bottomRightNode->right = NULL;
 
     return newMatrix;
 }
