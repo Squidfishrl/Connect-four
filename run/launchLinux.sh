20e77da738bbc5ba1e4d7059bea4b1c8e4bd096f
Squidfishrl <ivo.genchev4@gmail.com>
2021-06-24 09:10:04 +0300

    redid launch script
    works when being called from any dir

--- run/launchLinux.sh
+++ run/launchLinux.sh
@@ -1,24 +1,20 @@
-compile="gcc -Wall -pedantic -std=c11 "
-run="./temp.o"
-delOut="rm temp.o"
-baseDir=$(basename "$PWD") # get base directory (should be Connect_four or run)
-mainFile="main.c"
 
-if [ "$baseDir" = "Connect-four" ]
-then
-    pathToSrc="./src"
-elif [ "$baseDir" = "run" ]
-then
-    pathToSrc="../src"
-else
-    echo -e "ERROR - unknown directory\ndirectory name should be Connect_four or Connect_four/run"
-    exit 1
-fi
+# redirect stdout and stderr to /dev/null and then get pwd
+# get dir of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PATH_TO_SRC="../src"
+MAIN_FILE_NAME="main.c"
+OUT_FILE_NAME="temp.o"
+COMPILER="gcc -Wall -pedantic -std=c11 "
+EXECUTE_OUT="./$OUT_FILE_NAME"
+DELETE_OUT="rm $OUT_FILE_NAME"
 
-cd $pathToSrc # game runs only when its ran from ./src
-eval "$compile$mainFile -o temp.o"
-$run
-$delOut
+# if [ -z ${$1+x} ] # take absolute math
+# then
+
+cd $SCRIPT_DIR # cd to dir of the script
+cd $PATH_TO_SRC # cd to src - game doesnt work when ran from a different directory
+eval "$COMPILER$MAIN_FILE_NAME -o $OUT_FILE_NAME"
+$EXECUTE_OUT
+$DELETE_OUT
 # Dont have to go back to original dir cuz the script is ran in a subshell and is returned to oriwingal wd when it ends
-
-exit 0 # 0 is true in bash incase I forget
