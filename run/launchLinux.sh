555d0c863763b72cd1afe26aab691243ba7c0abc
Squidfishrl <ivo.genchev4@gmail.com>
2021-06-24 09:26:17 +0300

    finished launch script

--- run/launchLinux.sh
+++ run/launchLinux.sh
@@ -1,7 +1,4 @@
-
-# redirect stdout and stderr to /dev/null and then get pwd
-# get dir of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 PATH_TO_SRC="../src"
 MAIN_FILE_NAME="main.c"
 OUT_FILE_NAME="temp.o"
@@ -9,11 +6,12 @@ COMPILER="gcc -Wall -pedantic -std=c11 "
 EXECUTE_OUT="./$OUT_FILE_NAME"
 DELETE_OUT="rm $OUT_FILE_NAME"
 
-# if [ -z ${$1+x} ] # take absolute math
-# then
-
+ # Navigate to correct dir
 cd $SCRIPT_DIR # cd to dir of the script
 cd $PATH_TO_SRC # cd to src - game doesnt work when ran from a different directory
+
+ # Compile and run file
+
 eval "$COMPILER$MAIN_FILE_NAME -o $OUT_FILE_NAME"
 $EXECUTE_OUT
 $DELETE_OUT
