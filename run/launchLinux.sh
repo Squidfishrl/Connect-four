2950d6c976b72142bb1f006d4cb287bde9f63caf
Squidfishrl <ivo.genchev4@gmail.com>
2021-06-24 08:45:27 +0300

    fixed launch script being in wrong directory

--- run/launchLinux.sh
+++ run/launchLinux.sh
@@ -2,20 +2,23 @@ compile="gcc -Wall -pedantic -std=c11 "
 run="./temp.o"
 delOut="rm temp.o"
 baseDir=$(basename "$PWD") # get base directory (should be Connect_four or run)
+mainFile="main.c"
 
-if [ "$baseDir" = "Connect_four" ]
+if [ "$baseDir" = "Connect-four" ]
 then
-    pathToMain="src/main.c"
+    pathToSrc="./src"
 elif [ "$baseDir" = "run" ]
 then
-    pathToMain="../src/main.c"
+    pathToSrc="../src"
 else
     echo -e "ERROR - unknown directory\ndirectory name should be Connect_four or Connect_four/run"
     exit 1
 fi
 
-eval "$compile$pathToMain -o temp.o"
+cd $pathToSrc # game runs only when its ran from ./src
+eval "$compile$mainFile -o temp.o"
 $run
 $delOut
+# Dont have to go back to original dir cuz the script is ran in a subshell and is returned to oriwingal wd when it ends
 
 exit 0 # 0 is true in bash incase I forget
